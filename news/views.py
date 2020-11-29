59faa7a8f2bc284ef854ccf0f610f933ae34fa7c
kimja01 <kimja7045@naver.com>
2020-11-29 18:42:00 +0900

    Add crawling urlList

--- news/views.py
+++ news/views.py
@@ -10,30 +10,42 @@ from konlpy.tag import Okt
 from collections import Counter
 import matplotlib.pyplot as plt
 
-html = requests.get('http://www.dailypop.kr/news/articleList.html')
-soup = BeautifulSoup(html.content, 'html.parser')
-url_list = soup.find(class_='article-list').find_all('a')
-for link in url_list:
-    # print(link.text.strip())
-    print(link.get('href')+'\n')
+f = open('url.txt', 'w')
+page_counter = 1
+url_count = 0
 
-res = requests.get('http://www.dailypop.kr/news/articleView.html?idxno=47220')
-soup = BeautifulSoup(res.content, 'html.parser')
+for i in range(1, 6):
+    html = requests.get(f'http://www.dailypop.kr/news/articleList.html?page={i}&total=168&sc_section_code'
+                        '=&sc_sub_section_code=&sc_serial_code=&sc_area=A&sc_level=&sc_article_type=&sc_view_level'
+                        '=&sc_sdate=&sc_edate=&sc_serial_number=&sc_word=%EC%A7%91%EC%BD%95')
+    soup = BeautifulSoup(html.content, 'html.parser')
+    url_list = soup.find(class_='article-list-content text-left').find_all('a')
 
-title = soup.find('title')
-body = soup.find(id='article-view-content-div').find_all('p')
-images = soup.find(id='article-view-content-div').find_all('img')
-imgs = []
-for i, image in enumerate(images):
-    # print('http://www.dailypop.kr'+image.get('src'))
-    if(i!=6):
-        imgs.append('http://www.dailypop.kr'+image.get('src'))
+    for link in url_list:
+        if url_count == 99:
+            f.write('http://www.dailypop.kr/'+link.get('href'))
+        else:
+            f.write('http://www.dailypop.kr/' + link.get('href')+'\n')
+        url_count += 1
+f.close()
 
-content = []
-stringTypeContent = ''
-for i, e in enumerate(body):
-    content.append(e.get_text())
-    stringTypeContent += e.get_text()
+# res = requests.get('http://www.dailypop.kr/news/articleView.html?idxno=47220')
+# soup = BeautifulSoup(res.content, 'html.parser')
+
+# title = soup.find('title')
+# body = soup.find(id='article-view-content-div').find_all('p')
+# images = soup.find(id='article-view-content-div').find_all('img')
+# imgs = []
+# for i, image in enumerate(images):
+#     # print('http://www.dailypop.kr'+image.get('src'))
+#     if(i!=6):
+#         imgs.append('http://www.dailypop.kr'+image.get('src'))
+#
+# content = []
+# stringTypeContent = ''
+# for i, e in enumerate(body):
+#     content.append(e.get_text())
+#     stringTypeContent += e.get_text()
 
 # wc = WordCloud(font_path='/Library/Fonts/HMKMMAG.TTF', background_color='white', width=500, height=500, max_words=20, max_font_size=100)
 # okt = Okt()
